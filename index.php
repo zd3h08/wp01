cbabd0f854b6b86d852269e067daae1e29dd1e21
Shirakawa Saori <zd3h08@gmail.com>
2020-03-02 10:19:17 +0900

    見出しの追加

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
       <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
       <article class="post">
-        <h2>
+        <h2>かきくけこ
           <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
         </h2>
         <p class="postmeta">
