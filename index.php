eab8351f2447d2f332954cd1526070f0642cd771
Shirakawa Saori <zd3h08@gmail.com>
2020-03-02 10:25:43 +0900

    見出しの削除

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
       <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
       <article class="post">
-        <h2>かきくけこ
+        <h2>
           <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
         </h2>
         <p class="postmeta">
